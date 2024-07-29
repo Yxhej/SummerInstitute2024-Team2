47aa3f51722cb38aa12cae525df6842a54293738
Chloe Jarrett <jarrettc@BXSCI.78X445.NYCBOE.ORG>
2024-07-29 11:59:34 -0400

    added pidcontroller

--- src/main/java/frc/robot/subsystems/shooter.java
+++ src/main/java/frc/robot/subsystems/shooter.java
@@ -5,7 +5,9 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.Encoder;
 import frc.robot.Ports.IntakePorts;
 
@@ -13,7 +15,8 @@ public class shooter extends SubsystemBase {
     private final CANSparkMax motor = new CANSparkMax(7,MotorType.kBrushless);
     private double power; 
     private final DigitalInput hopperBeamSensor = new DigitalInput (IntakePorts.intakeBeam);
-    private final DigitalInput limit = new DigitalInput (5);
+    private RelativeEncoder pivotEncoder; 
+    private PIDController motorController = new PIDController (kp1, ki1, kd1);
 
     public void periodic() {
         //hopper
@@ -26,23 +29,12 @@ public class shooter extends SubsystemBase {
       }
     
     public void turnOn(){
-        motor.setVoltage();
+        double motorSpeed = pivotEncoder.getVelocity();
+        double v = motorController.calculate(motorSpeed, );
+        motor.setVoltage(v);
     }
 
     public void turnOff(){
         motor.set(0);
     }
-
-    public void Overshoot() {
-        power -= 0.001;
-    }
-
-    public void UnderShoot() {
-        power += 0.001;
-    }
-
-    public void Record() {
-        System.out.println(power);
-    }
-;
 }
