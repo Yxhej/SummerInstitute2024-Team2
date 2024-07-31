02c3d1c7f94c434989402482b938aecf49d1b514
chloej80 <chloej80@nycstudents.net>
2024-07-31 10:36:52 -0400

    added port

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 public final class Ports {
+    public static final int CONTROLLER = -1;
     public static final class IntakePorts{
         public static final int intakeBeam = -1;
     }  
