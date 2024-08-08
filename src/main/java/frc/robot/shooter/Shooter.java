a05e6249febb7c30ce176c305de2b062d36b61d1
Your Name <chloej80@nycstudents.net>
2024-08-08 09:36:45 -0400

    assigned relative encoder a value

--- src/main/java/frc/robot/shooter/Shooter.java
+++ src/main/java/frc/robot/shooter/Shooter.java
@@ -9,16 +9,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
   private final CANSparkMax motor = new CANSparkMax(7, MotorType.kBrushless);
-  private RelativeEncoder pivotEncoder;
+  private RelativeEncoder shooterwheelEncoder ;
   private PIDController motorController =
       new PIDController(ShooterConstants.kp1, ShooterConstants.ki1, ShooterConstants.kd1);
 
-  public void periodic() {}
+  public Shooter (){
+    shooterwheelEncoder = motor.getEncoder();
+  }
 
   public Command goTo(double speed) {
     return run(
         () -> {
-          double motorSpeed = pivotEncoder.getVelocity();
+          double motorSpeed = shooterwheelEncoder.getVelocity();
           double v = motorController.calculate(motorSpeed, speed);
           motor.setVoltage(v);
         }).finallyDo(() -> motor.setVoltage(0));
